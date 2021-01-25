32c2c627f56cd54fc3f651a558d8a8d56767ccaa
anthonyChaussin-d4e <anthony@bureau-romand.ch>
2021-01-25 22:05:28 +0100

    Ajoutez des fichiers projet.

---
+++ CardVisitReader/VisitCard.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Tesseract;
+
+namespace CardVisitReader
+{
+    /// <summary>
+    /// Logique d'interaction pour VisitCard.xaml
+    /// </summary>
+    public partial class VisitCard : UserControl, INotifyPropertyChanged
+    {
+        private string text;
+
+        public string File { get { return PictureFile.FullName; } }
+        public string CName { get { return Regex.Match(this.Text + "", @"([a-zA-Z]{2,}\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\s?([a-zA-Z]{1,})?)").Value.Split('\n')[0]; } }
+        public string Email { get { return Regex.Match(this.Text + "", @"[\w-\.]+@([\w-]+\.)+[\w-]{2,4}").Value.Split('\n')[0]; } }
+        public string Phone { get { return Regex.Match(this.Text + "", @"[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*").Value.Split('\n')[0]; } }
+        public string Adresse { get; set; }
+        public string Web { get { return Regex.Match(this.Text + "", @"(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+").Value.Replace("@", ".").Split('\n')[0]; } }
+        public string Twiter { get; set; }
+
+        public string Entreprise
+        {
+            get
+            {
+                List<string> lines = this.Text?.Split('\n')?.ToList();
+                if (lines != null && lines.Count > 0)
+                {
+                    return lines[lines.IndexOf(lines.Where(l => l.Contains(this.CName)).First()) + 1];
+                }
+                else
+                {
+                    return "";
+                }
+            }
+        }
+
+        public FileInfo PictureFile { get; set; }
+
+        public string Text
+        {
+            get => text;
+            set
+            {
+                text = value;
+                this.OnPropertyChanged(nameof(this.Web));
+                this.OnPropertyChanged(nameof(this.Twiter));
+                this.OnPropertyChanged(nameof(this.Adresse));
+                this.OnPropertyChanged(nameof(this.Email));
+                this.OnPropertyChanged(nameof(this.Phone));
+                this.OnPropertyChanged(nameof(this.CName));
+                this.OnPropertyChanged(nameof(this.Entreprise));
+            }
+        }
+
+        public event EventHandler CardDlicked;
+
+        /// <summary>
+        /// Event when a property change
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public VisitCard()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void ReadCard()
+        {
+            try
+            {
+                using (TesseractEngine engine = new TesseractEngine(@"./tessdata", "fra", EngineMode.Default))
+                {
+                    try
+                    {
+                        using (Pix img = Pix.LoadFromFile(PictureFile.FullName))
+                        {
+                            try
+                            {
+                                using (Tesseract.Page page = engine.Process(img))
+                                {
+                                    try
+                                    {
+                                        this.Text = page.GetText();
+                                    }
+                                    catch (Exception e)
+                                    {
+                                        Console.WriteLine("Unexpected Error: " + e.Message);
+                                        Console.WriteLine("Details: ");
+                                        Console.WriteLine(e.ToString());
+                                    }
+                                }
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine("Unexpected Error: " + e.Message);
+                                Console.WriteLine("Details: ");
+                                Console.WriteLine(e.ToString());
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Unexpected Error: " + e.Message);
+                        Console.WriteLine("Details: ");
+                        Console.WriteLine(e.ToString());
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected Error: " + e.Message);
+                Console.WriteLine("Details: ");
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        internal void ForceLoad()
+        {
+            this.UserControl_Loaded(this, null);
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.ReadCard();
+        }
+
+        /// <summary>
+        /// Treat the event of a property change
+        /// </summary>
+        /// <param name="name"> Name of the property </param>
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private void Card_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.CardDlicked?.Invoke(this, e);
+        }
+    }
+}
---
+++ Visit Card Reader/MainWindow.xaml.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Visit_Card_Reader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private VisitCard selectedCard;
+
+        public string Entreprise { get { return this.SelectedCard?.Entreprise; } }
+        public string CName { get { return this.SelectedCard?.CName; } }
+        public string Email { get { return this.SelectedCard?.Email; } }
+        public string Phone { get { return this.SelectedCard?.Phone; } }
+        public string Adresse { get { return this.SelectedCard?.Adresse; } }
+        public string Web { get { return this.SelectedCard?.Web; } }
+        public string Twiter { get { return this.SelectedCard?.Twiter; } }
+
+        public VisitCard SelectedCard
+        {
+            get => selectedCard;
+            set
+            {
+                selectedCard = value;
+                this.OnPropertyChanged(nameof(this.Web));
+                this.OnPropertyChanged(nameof(this.Twiter));
+                this.OnPropertyChanged(nameof(this.Adresse));
+                this.OnPropertyChanged(nameof(this.Email));
+                this.OnPropertyChanged(nameof(this.Phone));
+                this.OnPropertyChanged(nameof(this.Entreprise));
+                this.OnPropertyChanged(nameof(this.CName));
+            }
+        }
+
+        private string FolderPicturePath
+        {
+            get
+            {
+                return Properties.Settings.Default.PictureFolder;
+            }
+            set
+            {
+                Properties.Settings.Default.PictureFolder = value;
+                Properties.Settings.Default.Save();
+                this.LoadCards();
+            }
+        }
+
+        private string CSVFilePath
+        {
+            get
+            {
+                return Properties.Settings.Default.CSVFile;
+            }
+            set
+            {
+                Properties.Settings.Default.CSVFile = value;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        /// <summary>
+        /// Event when a property change
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void LoadCards()
+        {
+            new DirectoryInfo(this.FolderPicturePath).GetFiles("*", new EnumerationOptions() { IgnoreInaccessible = true, RecurseSubdirectories = true }).ToList().ForEach(file =>
+            {
+                VisitCard card = new VisitCard() { PictureFile = file };
+                card.CardDlicked += this.ClickCard;
+                this.CardList.Items.Add(card);
+            }
+            );
+        }
+
+        private void ClickCard(object sender, EventArgs e)
+        {
+            this.SelectedCard = (VisitCard)sender;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender == this.CSV)
+            {
+                using (CommonOpenFileDialog dialog = new CommonOpenFileDialog() { IsFolderPicker = true })
+                {
+                    if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+                    {
+                        this.CSVFilePath = dialog.FileName + @"\VistiCardData.csv";
+                    }
+                }
+            }
+            else if (sender == this.Picture)
+            {
+                this.CardList.Items.Clear();
+                using (CommonOpenFileDialog dialog = new CommonOpenFileDialog() { IsFolderPicker = true })
+                {
+                    if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+                    {
+                        this.FolderPicturePath = dialog.FileName;
+                    }
+                }
+            }
+            else if (sender == this.CreateCSV)
+            {
+                List<string> finalLines = new List<string>();
+                Mouse.OverrideCursor = Cursors.Wait;
+                this.CardList.Items.OfType<VisitCard>().ToList().ForEach(c => c.ForceLoad());
+                string firstLine = "Nom;Email;Phone;Web;Image";
+                List<string> lines = this.CardList.Items.OfType<VisitCard>().ToList().ConvertAll(c => c.Name.Replace("\n", "").Replace(";", ",") + ";" + c.Email.Replace("\n", "").Replace(";", ",") + ";" + c.Phone.Replace("\n", "").Replace(";", ",") + ";" + c.Web.Replace("\n", "").Replace(";", ",") + ";" + c.File);
+                if (File.Exists(this.CSVFilePath))
+                {
+                    finalLines.AddRange(File.ReadAllLines(this.CSVFilePath).ToList());
+                    File.Delete(this.CSVFilePath);
+                }
+                else
+                {
+                    finalLines.Add(firstLine);
+                }
+                finalLines.AddRange(lines);
+                File.WriteAllText(this.CSVFilePath, string.Join("\n", finalLines.Distinct()));
+                Mouse.OverrideCursor = null;
+            }
+            else
+            {
+                throw new Exception("Erreur action inconue");
+            }
+        }
+
+        /// <summary>
+        /// Treat the event of a property change
+        /// </summary>
+        /// <param name="name"> Name of the property </param>
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
